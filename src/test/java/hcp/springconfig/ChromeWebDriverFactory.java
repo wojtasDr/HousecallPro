146c73f78cc831464a4a76b13d04988c42903e39
Pulawski, Wojciech <wojciech.pulawski@gmail.com>
2023-03-14 09:14:25 +0100

    options added to chromedriver

--- src/test/java/hcp/springconfig/ChromeWebDriverFactory.java
+++ src/test/java/hcp/springconfig/ChromeWebDriverFactory.java
@@ -2,6 +2,7 @@ package hcp.springconfig;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,9 @@ public class ChromeWebDriverFactory {
     @Profile("chrome")
     public WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver", new File(chromeDriverLocation).getAbsolutePath());
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         return driver;
